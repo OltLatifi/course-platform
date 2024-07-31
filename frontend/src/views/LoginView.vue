8cc4b3c802871a56d9cb5c29748f3926129ac37b
System Administrator <oltlatifi2003@gmail.com>
2024-07-31 22:14:54 +0200

    Vee-validate setup

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<template>
+    <Form v-slot="{ values }">
+        <Field name="email" type="email" />
+        <Field name="password" type="password" />
+
+        <pre>{{ values }}</pre>
+    </Form>
+</template>
+<script setup>
+    import { Form, Field } from 'vee-validate';
+</script>
