e6c8f2698a57423d6fa396697d62c3ee68c18331
System Administrator <oltlatifi2003@gmail.com>
2024-08-02 21:48:56 +0200

    Clean up project and minor fixes to the UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
     import { RouterLink, RouterView } from 'vue-router'
-    import Footer  from './components/Footer.vue'
-    import NavBar  from './components/NavBar.vue'
+    import Footer from './components/Footer.vue'
+    import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-    <RouterView />
     <NavBar />
+    <div class="min-h-screen w-[96%] mx-auto">
+        <RouterView />
+    </div>
     <Footer />
 </template>
